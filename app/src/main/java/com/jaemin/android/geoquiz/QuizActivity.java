7d7539a516582c37c82465ce4769d32cc5dcea35
Jaemin <jaemin0216@gmail.com>
2016-09-28 20:30:32 +0900

    Connect the code with new Button

--- app/src/main/java/com/jaemin/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/jaemin/android/geoquiz/QuizActivity.java
@@ -47,5 +47,15 @@ public class QuizActivity extends AppCompatActivity {
                 Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
             }
         });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                int question = mQuestionBank[mCurrentIndex].getTextRedId();
+                mQuestionTextView.setText(question);
+            }
+        });
     }
 }

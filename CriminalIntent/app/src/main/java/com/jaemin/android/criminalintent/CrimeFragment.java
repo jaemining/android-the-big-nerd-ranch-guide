00755bce9601b9a259cf5ab44cb1d88fcc1aae05
Jaemin <jaemin0216@gmail.com>
2016-10-27 21:56:25 +0900

    Add instance variables and Set text and listener on each widgets

--- CriminalIntent/app/src/main/java/com/jaemin/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/jaemin/android/criminalintent/CrimeFragment.java
@@ -7,6 +7,9 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 /**
@@ -16,6 +19,8 @@ import android.widget.EditText;
 public class CrimeFragment extends Fragment { // Fragment의 서브 클래스로 변경하기
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,6 +48,19 @@ public class CrimeFragment extends Fragment { // Fragment의 서브 클래스로
             @Override
             public void afterTextChanged(Editable editable) {
                 // 이 메서드의 실행 코드는 여기서 필요 없음
+            }
+        });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false); // 버튼 비활성화
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // 범죄 해결 여부 속성 값을 설정한다
+                mCrime.setSolved(isChecked);
             }
         });
 

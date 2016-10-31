19b405001d06502ac786c85f46e80e8aa617b6ca
Jaemin <jaemin0216@gmail.com>
2016-10-31 20:23:29 +0900

    Create CrimeListFragment class

---
+++ CriminalIntent/app/src/main/java/com/jaemin/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.jaemin.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Jaemin on 2016. 10. 31..
+ */
+
+public class CrimeListFragment extends Fragment {
+}

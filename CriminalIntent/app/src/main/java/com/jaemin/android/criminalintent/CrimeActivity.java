e6c9089866568b3aec12cc75127b6299031a7670
Jaemin <jaemin0216@gmail.com>
2016-11-10 00:27:52 +0900

    Make intent and send data using putExtra

--- CriminalIntent/app/src/main/java/com/jaemin/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/jaemin/android/criminalintent/CrimeActivity.java
@@ -1,11 +1,23 @@
 package com.jaemin.android.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity { // 추상클래스 사용하기
+
+    public static final String EXTRA_CRIME_ID = "com.jaemin.android.criminalintent.crime_id";
+
+    public static Intent newIntent(Context packageContext, UUID crimeID) {
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeID);
+        return intent;
+    }
 
     @Override
     protected Fragment createFragment() {

33d879a3661beb88616ca1fcf3a03e43a5b657e2
Jaemin <jaemin0216@gmail.com>
2016-11-10 00:26:41 +0900

    Bring Activity using intent from fragment

--- CriminalIntent/app/src/main/java/com/jaemin/android/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/jaemin/android/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.jaemin.android.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -39,7 +41,7 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView.setAdapter(mAdapter);
     }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder { // 간단하게 ViewHolder 정의
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener { // 간단하게 ViewHolder 정의
         private Crime mCrime;
 
         private TextView mTitleTextView;
@@ -48,6 +50,7 @@ public class CrimeListFragment extends Fragment {
 
         public CrimeHolder(View itemView) {
             super(itemView);
+            itemView.setOnClickListener(this);
 
             mTitleTextView = (TextView) itemView.findViewById(R.id.list_item_crime_title_text_view);
             mDateTextView = (TextView) itemView.findViewById(R.id.list_item_crime_date_text_view);
@@ -59,6 +62,12 @@ public class CrimeListFragment extends Fragment {
             mTitleTextView.setText(mCrime.getTitle());
             mDateTextView.setText(mCrime.getDate().toString());
             mSolvedCheckBox.setChecked(mCrime.isSolved());
+        }
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getActivity(), CrimeActivity.class);
+            startActivity(intent);
         }
     }
 

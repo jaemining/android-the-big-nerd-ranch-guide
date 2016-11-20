ef17cf1acf87870d238a2825311d5bac73537b0b
Jaemin <jaemin0216@gmail.com>
2016-11-20 21:36:47 +0900

    Create CrimeDbSchema in database package

---
+++ CriminalIntent/app/src/main/java/com/jaemin/android/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,18 @@
+package com.jaemin.android.criminalintent.database;
+
+/**
+ * Created by Jaemin on 2016. 11. 20..
+ */
+
+public class CrimeDbSchema {
+    public static final class CrimeTable { // 내부 클래스
+        public static final String NAME = "crimes";
+
+        public static final class Cols { // 테이블의 열 정의 
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}

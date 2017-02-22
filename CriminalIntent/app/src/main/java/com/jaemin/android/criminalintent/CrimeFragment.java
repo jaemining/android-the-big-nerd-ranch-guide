8a766ca9cbd92368f16534084aa9e0a77d74bf2a
Jaemin <jaemin0216@gmail.com>
2017-02-22 19:11:37 +0900

    Override onPause()
    - To save when CrimeFragment paused.

--- CriminalIntent/app/src/main/java/com/jaemin/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/jaemin/android/criminalintent/CrimeFragment.java
@@ -54,6 +54,13 @@ public class CrimeFragment extends Fragment { // Fragment의 서브 클래스로
     }
 
     @Override
+    public void onPause() {
+        super.onPause();
+
+        CrimeLab.get(getActivity()).updateCrime(mCrime);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
 

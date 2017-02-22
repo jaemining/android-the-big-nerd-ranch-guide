ef6191587631a688c67209ac7c3a15689eb2d103
Jaemin <jaemin0216@gmail.com>
2017-02-22 19:56:37 +0900

    Get one Crime data from database

--- CriminalIntent/app/src/main/java/com/jaemin/android/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/jaemin/android/criminalintent/CrimeLab.java
@@ -64,7 +64,21 @@ public class CrimeLab {
     }
 
     public Crime getCrime(UUID id) {
-        return null;
+        CrimeCursorWrapper cursor = queryCrimes(
+                CrimeTable.Cols.UUID + " = ?",
+                new String[] { id.toString() }
+        );
+
+        try {
+            if (cursor.getCount() == 0) {
+                return null;
+            }
+
+            cursor.moveToFirst();
+            return cursor.getCrime();
+        } finally {
+            cursor.close();
+        }
     }
 
     public void updateCrime(Crime crime) {
